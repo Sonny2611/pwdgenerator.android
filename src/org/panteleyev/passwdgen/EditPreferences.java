e83ab9d064c85fbdfee2326f8e1f071b7bebcc4e
Petr Panteleyev <petr@panteleyev.org>
2012-12-28 21:28:18 +0400

    Tabs -> spaces

--- src/org/panteleyev/passwdgen/EditPreferences.java
+++ src/org/panteleyev/passwdgen/EditPreferences.java
@@ -31,8 +31,8 @@ import android.preference.PreferenceActivity;
 
 public class EditPreferences extends PreferenceActivity {
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    addPreferencesFromResource(R.xml.preferences);
-	}	
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }	
 }
